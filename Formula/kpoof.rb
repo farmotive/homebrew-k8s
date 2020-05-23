335458e5617b36ecd5b1035a5cd073dde69ee98a
Josh Hull <josh.hull@lypht.com>
2020-05-23 07:40:48 -0500

    Update kpoof.rb

--- Formula/kpoof.rb
+++ Formula/kpoof.rb
@@ -1,8 +1,8 @@
 class Kpoof < Formula
   desc "Quick k8s pod port-forward utility"
   homepage "https://github.com/farmotive/kpoof"
-  url "https://github.com/farmotive/kpoof/archive/3.0.0.tar.gz"
-  sha256 "de692f7bd49b799fd584c3f457e647883e52372d369b1694f23a66b770f364c1"
+  url "https://github.com/farmotive/kpoof/archive/3.1.0.tar.gz"
+  sha256 "bbe3fa94c46c2db94895d820f01b50cabfe5e97048954d2bd35a7ba4876446f3"
 
   depends_on "kubernetes-cli"  => :recommended
 
